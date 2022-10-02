4c79e35d109803053671562b5d16e1dbb8d68512
Kai <kailat8901@gmail.com>
2022-10-02 16:38:57 +0200

    added documentation to ExceptionBuilder.java

--- src/main/java/com/github/klate/rps/exception/ExceptionBuilder.java
+++ src/main/java/com/github/klate/rps/exception/ExceptionBuilder.java
@@ -1,11 +1,16 @@
 package com.github.klate.rps.exception;
 
+/**
+ * class, that contains methods for building exceptions strings efficiently,
+ * by minimizing the String-heap allocations
+ * */
 public class ExceptionBuilder {
 
 
     /**
      * creates an exception message from the given values
-     * minmizes the String-heap allocations while doing it
+     * @param baseMessage the start of the message
+     * @param value the appended value to the base message
      * */
     public static String createMessage(String baseMessage, char value){
         StringBuilder builder = new StringBuilder(baseMessage.length() + 1);
@@ -14,6 +19,12 @@ public class ExceptionBuilder {
         return builder.toString();
     }
 
+    /**
+     * creates an exception message from the given values
+     * @param baseMessage the start of the message
+     * @param value1 the first appended value to the base message
+     * @param value2 the second appended value to the base message
+     * */
     public static String createMessage(String baseMessage, char value1, char value2) {
         StringBuilder builder = new StringBuilder(baseMessage.length() + 3);
         builder.append(baseMessage)
@@ -24,6 +35,13 @@ public class ExceptionBuilder {
         return builder.toString();
     }
 
+    /**
+     * creates an exception message from the given values
+     * @param baseMessage the start of the message
+     * @param params the first appended values to the base message (comma separated)
+     * @param addOnMessage the second message to be appended towards the params
+     * @param addOnParam the third message to be appended towards the end
+     * */
     public static String createMessage(String baseMessage, char[] params, String addOnMessage, char addOnParam){
         StringBuilder builder = new StringBuilder(baseMessage.length() + (params.length * 2) + addOnMessage.length() + 1);
         builder.append(baseMessage);
